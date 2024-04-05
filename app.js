030942e91984c46f5301d82f6e6e497bf792ebf3
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-05 08:54:36 +0200

    feat: fnish task

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req;
+  // main route
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assignment 01</title></head>");
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send user</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assignment 1</title></head>");
+    res.write("<body><ul><li>User 1</li><li>User 2</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-user") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+  }
 });
 
 server.listen(3000);
