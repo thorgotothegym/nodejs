1a3cacb5d41e0c43395176d245f934b6c4e356df
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-10 12:29:45 +0200

    new middleware and handle routes

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ const express = require("express");
 
 const app = express();
 
-const dummy = {
-  name: "panda",
-  food: "Bamboo",
-};
-
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next(); // i need to pass next() to go to next middleware
+app.use("/user", (req, res, next) => {
+  res.send("<h1>Dummy Response</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("second middleware");
   res.send("<h1>Welcome to Express</h1>");
-  /* res.send(dummy);
-  res.end(); */
 });
 
 app.listen(3000);
