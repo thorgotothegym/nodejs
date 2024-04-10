4387eccdf60c5cf67d295b9f06350b1b9e573da5
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-10 13:11:48 +0200

    feat: install bodyparse and create form to send book

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const bodyParse = require("body-parser");
 
 const app = express();
 
-app.use("/user", (req, res, next) => {
-  res.send("<h1>Dummy Response</h1>");
+app.use(bodyParse.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">send<button/></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("second middleware");
   res.send("<h1>Welcome to Express</h1>");
 });
 
