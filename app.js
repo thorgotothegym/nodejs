f7d4fb4537b33742bb89ea5b80590ee7ab9749a4
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-11 10:56:58 +0200

    small refactor in app

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const bodyParse = require("body-parser");
-
-const routesAdmin = require("./routes/admin");
-const routesShop = require("./routes/shop");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParse.urlencoded({ extended: false }));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/admin", routesAdmin);
-app.use(routesShop);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Welcome to Express</h1>");
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
