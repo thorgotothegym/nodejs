16b07db1769596f700ddbd5143525f13d5567056
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-11 10:13:57 +0200

    feat: extract routes and change paths

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 
+const routesAdmin = require("./routes/admin");
+const routesShop = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParse.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">send<button/></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("/admin", routesAdmin);
+app.use(routesShop);
 
 app.use("/", (req, res, next) => {
   res.send("<h1>Welcome to Express</h1>");
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routes = express.Router();
+
+routes.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/product" method="POST"><input type="text" name="title" /> <button type="submit">send<button/></form>'
+  );
+});
+
+routes.post("/add-product", (req, res, next) => {
+  res.redirect("/");
+});
+
+module.exports = routes;
