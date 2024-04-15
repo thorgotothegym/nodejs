e67090d8bf500c0bd1c426af1d11e8ecc880f33b
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-15 09:01:47 +0200

    feat: after post create a obj with title product and export router and products

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,22 @@
-const express = require("express");
 const path = require("path");
 
-const routes = express.Router();
+const express = require("express");
 
-routes.get("/add-product", (req, res, next) => {
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
-routes.post("/add-product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  console.log("title:", req.body.title);
   res.redirect("/");
 });
 
-module.exports = routes;
+exports.routes = router;
+exports.products = products;
