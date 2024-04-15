c41c4ec1da719732a07393bdbf083791f3bd40db
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-15 09:02:16 +0200

    feat: render shop.pug in /

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  // render shop === shop.pug in view
+  res.render("shop");
 });
 
 module.exports = router;
