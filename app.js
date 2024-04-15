789e16bcce4c858f06ee11540f58fa59172d079a
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-15 08:59:57 +0200

    feat: set up pug config change export admindata routes

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+// set pug
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
