4060c6ef864736d7e48a4f1f4c21fedb47c8ec61
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-16 11:39:47 +0200

    add standarjs and config eslint

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,25 @@
-const path = require("path");
+const express = require('express')
 
-const express = require("express");
+const router = express.Router()
 
-const router = express.Router();
-
-const products = [];
+const products = []
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true,
-  });
-});
+    activeAddProduct: true
+  })
+})
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title })
+  res.redirect('/')
+})
 
-exports.routes = router;
-exports.products = products;
+exports.routes = router
+exports.products = products
