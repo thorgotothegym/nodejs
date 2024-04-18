c365e8b3c98d7c20f97bca9301ee5630695b024a
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-18 12:22:46 +0200

    refactor: products - controllers

--- controller/products.js
+++ controller/products.js
@@ -11,19 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-  const products = new Product(req.body.title)
-  products.save()
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll()
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    })
   })
 }
