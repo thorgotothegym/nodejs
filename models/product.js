7b35e24e74445dc8603fc087d3373df5caa453b2
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-04-18 12:22:22 +0200

    refactor geOProdcutsFromFile and manager errors

--- models/product.js
+++ models/product.js
@@ -1,4 +1,20 @@
-const products = []
+const fs = require('node:fs')
+const path = require('node:path')
+
+const thePath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+)
+
+console.log(thePath)
+
+const getProductsFromFile = cb => {
+  fs.readFile(thePath, (err, fileContent) => {
+    // eslint-disable-next-line n/no-callback-literal
+    err ? cb([]) : cb(JSON.parse(fileContent))
+  })
+}
 
 module.exports = class Product {
   constructor (t) {
@@ -6,10 +22,15 @@ module.exports = class Product {
   }
 
   save () {
-    products.push(this)
+    getProductsFromFile(products => {
+      products.push(this)
+      fs.writeFile(thePath, JSON.stringify(products), (err) => {
+        err ? console.log('error', err) : console.log(`the ${JSON.stringify(this.title)} has been add in the db`)
+      })
+    })
   }
 
-  static fetchAll () {
-    return products
+  static fetchAll (cb) {
+    getProductsFromFile(cb)
   }
 }
